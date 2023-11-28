0fbae08dcc7a0cb1fa39033a5b8660e47a3619f8
Otto-12 <ot.tiitinen@gmail.com>
2023-11-28 21:04:26 +0200

    Uusi feature
    Lisätty feature, joka pakottaa käyttäjän vastaamaan jokaiseen kysymykseen. /modal ja close button/

--- client/testi.js
+++ client/testi.js
@@ -8,12 +8,40 @@ window.addEventListener("load", () => {
         document.body.removeChild(loader);
     });
 });
+
+function closeModal() {
+    document.getElementById('customModal').style.display = 'none';
+}
+
+// Attach event listeners after the DOM has fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    // Attach event listener to the close button
+    const closeButton = document.querySelector('.close-button');
+    if (closeButton) {
+        closeButton.addEventListener('click', closeModal);
+    }
+
+    // Click outside the modal to close it
+    window.onclick = function(event) {
+        const modal = document.getElementById('customModal');
+        if (event.target === modal) {
+            closeModal();
+        }
+    };
+});
 function submitAnswers() {
     // Hae arvot lomakkeesta
     const answer1 = document.getElementById('answer1').value;
     const answer2 = document.getElementById('answer2').value;
     const answer3 = document.getElementById('answer3').value;
     const answer4 = document.getElementById('answer4').value;
+    //pakotetaan käyttäjä täyttämään kaikki kohdat
+    if (!answer1 || !answer2 || !answer3 || !answer4) {
+        document.getElementById('customModal').style.display = 'block'; 
+        return; 
+    }
+   // Function to close the modal
+
 
     // Tallenna arvot paikalliseen varastoon
     localStorage.setItem('answer1', answer1);
